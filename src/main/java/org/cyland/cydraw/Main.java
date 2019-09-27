e744b1ee950932ef1151348435c06ac5b4184df8
Tukes <xtukesx@gmail.com>
2019-09-28 01:43:06 +0300

    Extracted hardcoded constants

--- src/main/java/org/cyland/cydraw/Main.java
+++ src/main/java/org/cyland/cydraw/Main.java
@@ -6,16 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import static org.cyland.cydraw.contract.Contract.VIEW_SLUG;
+
+final public class Main extends Application {
+
+  private final static String APPLICATION_NAME = "CyDraw";
+  private final static String VIEW_NAME = "/main_stage.fxml";
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("/view/main_stage.fxml"));
-    primaryStage.setTitle("CyDraw");
+    Parent root = FXMLLoader.load(getClass().getResource(VIEW_SLUG + VIEW_NAME));
+    primaryStage.setTitle(APPLICATION_NAME);
     primaryStage.setScene(new Scene(root));
     primaryStage.show();
   }
-
 
   public static void main(String[] args) {
     launch(args);
