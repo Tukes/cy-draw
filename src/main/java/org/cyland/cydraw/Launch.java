1b5ee7f2877fd551a691a75dc6816bf007adf921
Tukes <xtukesx@gmail.com>
2019-09-30 05:19:01 +0300

    Added base ui

--- src/main/java/org/cyland/cydraw/Launch.java
+++ src/main/java/org/cyland/cydraw/Launch.java
@@ -5,20 +5,27 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import static org.cyland.cydraw.contract.Contract.VIEW_SLUG;
 
-final public class Main extends Application {
+final public class Launch extends Application {
+
+  public static Stage stage;
 
   private final static String APPLICATION_NAME = "CyDraw";
   private final static String VIEW_NAME = "/main_stage.fxml";
 
   @Override
   public void start(Stage primaryStage) throws Exception {
+
     Parent root = FXMLLoader.load(getClass().getResource(VIEW_SLUG + VIEW_NAME));
 
+    primaryStage.initStyle(StageStyle.UNDECORATED);
     primaryStage.setTitle(APPLICATION_NAME);
     primaryStage.setScene(new Scene(root));
+
+    stage = primaryStage;
     primaryStage.show();
   }
 
